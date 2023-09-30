3c9f4ccd3dbf7711d1c9a0aeb9eca4c0fbb651af
YusuxTX <syusux@gmail.com>
2023-09-30 14:34:47 +0800

    feat: add lab1 part2 base

--- src/lab1/init/test.c
+++ src/lab1/init/test.c
@@ -4,5 +4,9 @@
 // Please do not modify
 
 void test() {
-    while (1);
+    while (1) {
+        printk("kernel is running!\n");
+        // wait for a while
+        for (int i = 0; i < 385000000; i++);
+    }
 }
