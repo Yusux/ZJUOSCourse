1a0300a8805ee6858a4eb22b04a69180a997b675
YusuxTX <syusux@gmail.com>
2023-10-02 23:30:53 +0800

    feat: finish lab1

--- src/lab1/init/test.c
+++ src/lab1/init/test.c
@@ -4,6 +4,11 @@
 // Please do not modify
 
 void test() {
+    long long a = csr_read(sstatus);
+    printk("sstatus = %llx\n", a);
+    csr_write(sscratch, 0x0123456789abcdef);
+    a = csr_read(sscratch);
+    printk("sscratch = %llx\n", a);
     while (1) {
         printk("kernel is running!\n");
         // wait for a while
